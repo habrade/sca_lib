da962b5f686d4ba2728f90d472e4cd5679f6552c
s.dong <s.dong@gsi.de>
2019-07-22 15:37:15 +0200

    remove threading, some lock problems

--- sca_srv.py
+++ sca_srv.py
@@ -20,6 +20,19 @@ class ScaSrv(Gdpb):
         self.__afck_num = afck_num
         self.__link = link
 
+        # Reset SCA
+        self.send_reset()
+        # Connect SCA chip
+        self.send_connect()
+        # Enable ADC
+        self.enable_chn(SCA_CH_ADC, True)
+        # Enable GPIO
+        self.enable_chn(SCA_CH_GPIO, True)
+        # Enable I2C ch. 0
+        self.enable_chn(SCA_CH_I2C0, True)
+        self.set_frq(SCA_I2C_SPEED_1000)
+        self.set_mode(SCA_I2C_MODE_OPEN_DRAIN)
+
         self.__PREFIX = "labtest:Gdpb:%d:SCA:%d:" % (self.__afck_num, self.__link)
 
         self.ca_sca_id = pvaccess.Channel(self.__PREFIX + "ID")
@@ -41,13 +54,9 @@ class ScaSrv(Gdpb):
             self.ca_adc_channels.append(pvaccess.Channel(self.__PREFIX + "ADC:CH:" + str(ch_index)))
 
         self.read_sca_modules_ids()
-        self.create_threads()
+        # self.create_threads()
 
     def read_sca_modules_ids(self):
-        # Reset Chip
-        self.send_reset()
-        # Connect SCA chip
-        self.send_connect()
         # read SCA ID
         self.enable_chn(SCA_CH_ADC, True)
         sca_id = self.read_sca_id()
@@ -75,85 +84,80 @@ class ScaSrv(Gdpb):
             thread.join()
 
     def gpio_thread_func(self):
-        self.enable_chn(SCA_CH_GPIO, True)
-        while True:
-            # GPIO Direction Set
-            direction_set_1 = self.ca_gpio_direction_set_ch_31_16.get().getInt()
-            direction_set_2 = self.ca_gpio_direction_set_ch_15_0.get().getInt()
-            direction_set = (direction_set_1 << 16) + direction_set_2
-            log.debug("GPIO Direction Set to %#x" % direction_set)
-            self.set_direction(direction_set)
-            time.sleep(0.01)
-            # GPIO Direction Get
-            direction_get = self.get_direction()
-            log.debug("GPIO Direction Get =  %#x" % direction_get)
-            self.ca_gpio_direction_get_ch_31_16.putUShort(direction_get >> 16)
-            self.ca_gpio_direction_get_ch_15_0.putUShort(direction_get & 0xFFFF)
-            time.sleep(0.01)
-            # GPIO PinOut Set
-            pinout_set_1 = self.ca_gpio_pinout_set_ch_31_16.get().getInt()
-            pinout_set_2 = self.ca_gpio_pinout_set_ch_15_0.get().getInt()
-            pinout_set = (pinout_set_1 << 16) + pinout_set_2
-            log.debug("GPIO PINOUT Set to %#x" % pinout_set)
-            self.write_pin_out(pinout_set)
-            time.sleep(0.01)
-            # GPIO PinOut Get
-            pinout_get = self.read_pin_out()
-            log.debug("GPIO PINOUT Get =  %#x" % pinout_get)
-            self.ca_gpio_pinout_get_ch_31_16.putUShort(pinout_get >> 16)
-            self.ca_gpio_pinout_get_ch_15_0.putUShort(pinout_get & 0xFFFF)
-            time.sleep(0.01)
-            # GPIO PinIn Get
-            pinin_get = self.read_pin_in()
-            log.debug("GPIO PININ Get =  %#x" % pinin_get)
-            self.ca_gpio_pinin_get_ch_31_16.putUShort(pinin_get >> 16)
-            self.ca_gpio_pinin_get_ch_15_0.putUShort(pinin_get & 0xFFFF)
+        # while True:
+        # GPIO Direction Set
+        direction_set_1 = self.ca_gpio_direction_set_ch_31_16.get().getInt()
+        direction_set_2 = self.ca_gpio_direction_set_ch_15_0.get().getInt()
+        direction_set = (direction_set_1 << 16) + direction_set_2
+        log.debug("GPIO Direction Set to %#x" % direction_set)
+        self.set_direction(direction_set)
+        time.sleep(0.01)
+        # GPIO Direction Get
+        direction_get = self.get_direction()
+        log.debug("GPIO Direction Get =  %#x" % direction_get)
+        self.ca_gpio_direction_get_ch_31_16.putUShort(direction_get >> 16)
+        self.ca_gpio_direction_get_ch_15_0.putUShort(direction_get & 0xFFFF)
+        time.sleep(0.01)
+        # GPIO PinOut Set
+        pinout_set_1 = self.ca_gpio_pinout_set_ch_31_16.get().getInt()
+        pinout_set_2 = self.ca_gpio_pinout_set_ch_15_0.get().getInt()
+        pinout_set = (pinout_set_1 << 16) + pinout_set_2
+        log.debug("GPIO PINOUT Set to %#x" % pinout_set)
+        self.write_pin_out(pinout_set)
+        time.sleep(0.01)
+        # GPIO PinOut Get
+        pinout_get = self.read_pin_out()
+        log.debug("GPIO PINOUT Get =  %#x" % pinout_get)
+        self.ca_gpio_pinout_get_ch_31_16.putUShort(pinout_get >> 16)
+        self.ca_gpio_pinout_get_ch_15_0.putUShort(pinout_get & 0xFFFF)
+        time.sleep(0.01)
+        # GPIO PinIn Get
+        pinin_get = self.read_pin_in()
+        log.debug("GPIO PININ Get =  %#x" % pinin_get)
+        self.ca_gpio_pinin_get_ch_31_16.putUShort(pinin_get >> 16)
+        self.ca_gpio_pinin_get_ch_15_0.putUShort(pinin_get & 0xFFFF)
 
     def adc_thread_func(self):
-        self.enable_chn(SCA_CH_ADC, True)
-        while True:
-            # read adc channels for 0 32
-            for i in range(32):
-                self.w_sel(i)
-                self.start_conv()
-                time.sleep(0.01)
-                adc_value = self.r_data()
-                # read internal tenperature sensor
-                if i == 31:
-                    internal_temp = (725 - adc_value) / 2
-                    log.debug("ADC Ch %d = %#x Temp = %f" % (i, adc_value, internal_temp))
-                    # not vert accurate number to caluate the internal temprature, the manual doesn't give a formular.
-                    self.ca_adc_channels[i].putDouble(internal_temp)
-                else:
-                    volt_value = float(1000 * adc_value * SCA_ADC_VREF) / (2 ** 12)
-                    log.debug("ADC Ch %d =  %#x Volt = %f" % (i, adc_value, volt_value))
-                    self.ca_adc_channels[i].putDouble(volt_value)
+        # while True:
+        # read adc channels for 0 32
+        for i in range(32):
+            self.w_sel(i)
+            self.start_conv()
+            time.sleep(0.01)
+            adc_value = self.r_data()
+            # read internal tenperature sensor
+            if i == 31:
+                internal_temp = (725 - adc_value) / 2
+                log.debug("ADC Ch %d = %#x Temp = %f" % (i, adc_value, internal_temp))
+                # not vert accurate number to caluate the internal temprature, the manual doesn't give a formular.
+                self.ca_adc_channels[i].putDouble(internal_temp)
+            else:
+                volt_value = float(1000 * adc_value * SCA_ADC_VREF) / (2 ** 12)
+                log.debug("ADC Ch %d =  %#x Volt = %f" % (i, adc_value, volt_value))
+                self.ca_adc_channels[i].putDouble(volt_value)
 
     def bme280_thread_func(self):
-        # Enable I2C ch. 0
-        self.enable_chn(SCA_CH_I2C0, True)
-        self.set_frq(SCA_I2C_SPEED_100)
-        self.set_mode(SCA_I2C_MODE_OPEN_DRAIN)
-        # reset bme280
-        self.rst_dev()
-        # check BME280 ID
-        if self.read_id() != 0x60:
-            log.error("BME280's ID is not right, should be 0x60 after reset")
-        while True:
-            degrees = self.read_temperature()
-            pascals = self.read_pressure()
-            hectopascals = pascals / 100
-            humidity = self.read_humidity()
-            # Data put to epics channel
-            self.ca_bme280_degrees.putDouble(degrees)
-            self.ca_bme280_hectopascals.putDouble(hectopascals)
-            self.ca_bme280_humidity.putDouble(humidity)
-            log.debug("Temp = %f deg C" % degrees)
-            log.debug("Pressure = %f hPa" % hectopascals)
-            log.debug("Humidity = %f %%" % humidity)
+        # while True
+        # time.sleep(1)
+        degrees = self.read_temperature()
+        pascals = self.read_pressure()
+        hectopascals = pascals / 100
+        humidity = self.read_humidity()
+        # Data put to epics channel
+        self.ca_bme280_degrees.putDouble(degrees)
+        self.ca_bme280_hectopascals.putDouble(hectopascals)
+        self.ca_bme280_humidity.putDouble(humidity)
+        log.debug("Temp = %f deg C" % degrees)
+        log.debug("Pressure = %f hPa" % hectopascals)
+        log.debug("Humidity = %f %%" % humidity)
 
 
 if __name__ == '__main__':
     afck_num = 66
     link = 0
     scaSrv = ScaSrv(afck_num, link)
+    while True:
+        scaSrv.gpio_thread_func()
+        scaSrv.adc_thread_func()
+        scaSrv.bme280_thread_func()
+
